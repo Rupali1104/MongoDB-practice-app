1e224b559b8310ca5a3a919ea27f6ecb599eac14
Rupali R <rupaliii739@gmail.com>
2024-09-14 00:32:11 +0530

    updated

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require("path");
 const userModel = require('./models/user');
 
+app.use(express.static(path.join(__dirname,"public")));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,"public")));
+
 
 app.set("view engine","ejs");
 
